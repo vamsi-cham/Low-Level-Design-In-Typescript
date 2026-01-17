dd7c67e9208c9c0d345b005600e41e2687b8309a
vamsi-cham <62752615+vamsi-cham@users.noreply.github.com>
2026-01-17 14:40:32 +0530

    refactor: remove old code

--- src/Enum/OrderStatus.ts
+++
@@ -1,4 +0,0 @@
-export enum OrderStatus{
-  'INPROCESS' = 'INPROCESS',
-  'DISPATCHED' = 'DISPATCHED'
-}
--- src/Enum/PickerAlgo.ts
+++
@@ -1,4 +0,0 @@
-export enum PickerAlgo{
-  'LOW_PRICE' = 'LOW_PRICE',
-  'FIRST_ITEM' = 'FIRST_ITEM'
-}
